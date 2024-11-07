ba4da19e2081e15581173ec7cc3a58e50c11f9a7
PikluDatta <pikludatta98@gmail.com>
2024-11-07 22:57:01 +0600

    third commit

--- Assignment/src/problem-5.ts
+++ Assignment/src/problem-5.ts
@@ -1,3 +1,12 @@
 {
-    
+   const getProperty = <T,Q extends keyof T> (obj : T , key:Q )=>{
+      return obj[key]
+   }
+
+   const person = {
+    name: "Alice",
+     age: 30
+   }
+
+   console.log(getProperty(person,"name"));
 }
