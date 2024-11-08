04ecf52ce9e02bd4f23405eb83e6a40de07e9e65
PikluDatta <pikludatta98@gmail.com>
2024-11-08 08:45:25 +0600

    fifth commit

--- Assignment/src/problem-6.ts
+++ Assignment/src/problem-6.ts
@@ -1,3 +1,26 @@
 {
-    
+    interface Profile {
+        name:string,
+        age:number,
+        email:string
+    }
+
+    const updateProfile = (profile:Profile , updates:{
+        name?:string
+        age?:number
+        email?:string
+
+    }):Profile=>{
+       return {
+        name:updates.name?? profile.name,
+        age:updates.age?? profile.age,
+        email:updates.email??profile.email
+       }
+
+
+    }
+
+    const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(updateProfile(myProfile, { age: 29 }));
+
 }
